72b9511612d02be513e0aa9c528bda15c2b37962
trantrongdai <trongdai306@gmail.com>
2024-10-27 10:38:38 +0700

    Update api.ts

--- shorted-fe/app/service/api.ts
+++ shorted-fe/app/service/api.ts
@@ -9,7 +9,7 @@ import { ITask } from "../../types/tasks";
 //const baseUrlInside = 'http://192.168.1.8:8080';
 
 // call in server
-const baseUrlOut = 'http://34.87.97.87:8080';
+const baseUrlOut = 'http://34.124.150.106:8080';
 const baseUrlInside = 'http://shorted-be:8080';
 
 export const getAllTodos = async (): Promise<ITask[]> => {
