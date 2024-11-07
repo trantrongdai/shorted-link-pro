6f836e7e2d5263b84d2066d32bc22ae26cf487fb
trantrongdai <trongdai306@gmail.com>
2024-11-07 15:26:52 +0700

    Update api.ts

--- shorted-fe/app/service/api.ts
+++ shorted-fe/app/service/api.ts
@@ -8,7 +8,7 @@ import { ITask } from "../../types/tasks";
 //const baseUrlOut = 'http://localhost:8080';
 //const baseUrlInside = 'http://192.168.1.8:8080';
 
-// test webhook
+// test webhook update
 
 // call in server
 const baseUrlOut = 'http://34.124.150.106:8080';
