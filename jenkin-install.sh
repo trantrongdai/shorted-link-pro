1546883b7691d390dc1f9dcc011f3da02c99bbfb
Tony Tran <trongdai306@gmail.com>
2025-01-12 16:13:35 +0700

    update

--- jenkin-install.sh
+++ jenkin-install.sh
@@ -25,3 +25,28 @@ sudo apt-get install jenkins -y
 
 #get password:
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
+# Set up Docker's apt repository.
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# 2. Install the Docker packages.
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+
+# 3. Verify that the Docker Engine installation is successful by running the hello-world image.
+sudo docker run hello-world
+
+# 6. open sudo permission for docker in jenkin:
+sudo chmod 666 /var/run/docker.sock
