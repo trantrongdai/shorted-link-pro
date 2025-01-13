290317f71eec6ca9af65eb04a8efea46f52ca65c
Tony Tran <trongdai306@gmail.com>
2025-01-13 10:55:56 +0700

    no message

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
@@ -34,7 +34,7 @@ public class LimitsController {
                 configuration.getMaximum());
     }
 
-    // test 9
+    // test 10
     @PostMapping("/shortedLinks")
     public ResponseEntity addLink(@RequestBody ShortedLinkRequestDto shortedLinkRequestDto) {
         if(!RegexUtil.isMatch(shortedLinkRequestDto.getLinkOriginal(), ShortedLinkConstants.HTTP_REGEX)) {
